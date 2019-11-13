24d6ea982d7b6a2b5d555b28f482f866860901c8
Kevin Kirby <kkirby16@my.whitworth.edu>
2019-11-13 09:46:04 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -14,9 +14,8 @@ def print_first_directors_movie_titles
   while row_index < directors_database.count do 
     element_index = 0 
     while element_index < directors_database[row_index].count do 
-      print directors_database[row_index][element_index]
+      puts directors_database[row_index][element_index]
       element_index += 1
   end
   row_index += 1 
-end 
 end 
