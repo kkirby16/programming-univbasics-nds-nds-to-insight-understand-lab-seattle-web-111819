332226ec15c4c19d921de15e9c43e39997d163b9
Kevin Kirby <kkirby16@my.whitworth.edu>
2019-11-13 09:57:23 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,7 +9,7 @@ def pretty_print_nds(nds)
   nil
 end
 
-def print_first_directors_movie_titles(nds)
+def print_first_directors_movie_titles
   row_index = 0 
   while row_index < directors_database.count do 
     element_index = 0 
