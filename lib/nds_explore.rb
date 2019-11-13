a9dab0c34916fbae65517076a0782281e33c8fde
Kevin Kirby <kkirby16@my.whitworth.edu>
2019-11-13 09:35:34 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,9 +11,14 @@ end
 
 def print_first_directors_movie_titles
   row_index = 0 
-  while row_index < print_f
-  
-end
+  while row_index < print_first_directors_movie_titles.count do 
+    element_index = 0 
+    while element_index < print_first_directors_movie_titles[row_index].count do 
+      puts print_first_directors_movie_titles[row_index][element_index]
+      element_index += 1
+  end
+  row_index += 1 
+end 
 
 require 'pp'
 
